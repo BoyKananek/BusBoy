1e69346360b43200b204fd12196c0e9c7ef31717
Kan-anek <kananek.ati@hotmail.com>
2017-03-14 16:26:10 +0700

    change and fix bugs

--- index.js
+++ index.js
@@ -1,120 +1,54 @@
-var express = require('express');
+var express = require('express');  
+var bodyParser = require('body-parser');  
+var request = require('request');  
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));  
+app.use(bodyParser.json());  
 var port = process.env.PORT || 3000;
 
-app.get('/',function(req,res){
-    console.log("Home");
-    res.end("Chatbot");
-})
-
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === 'busboybot') {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
+// Server frontpage
+app.get('/', function (req, res) {  
+    res.send('This is TestBot Server');
 });
 
-app.post('/webhook', function (req, res) {
-  var data = req.body;
-
-  // Make sure this is a page subscription
-  if (data.object === 'BusBoy') {
-
-    // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function(entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
-
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.message) {
-          receivedMessage(event);
-        } else {
-          console.log("Webhook received unknown event: ", event);
-        }
-      });
-    });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know
-    // you've successfully received the callback. Otherwise, the request
-    // will time out and we will keep trying to resend.
-    res.sendStatus(200);
-  }
-});
-  
-function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
-
-  console.log("Received message for user %d and page %d at %d with message:", 
-    senderID, recipientID, timeOfMessage);
-  console.log(JSON.stringify(message));
-
-  var messageId = message.mid;
-
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
-
-  if (messageText) {
-
-    // If we receive a text message, check to see if it matches a keyword
-    // and send back the example. Otherwise, just echo the text we received.
-    switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-
-      default:
-        sendTextMessage(senderID, messageText);
-    }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
-  }
-}
-
-function sendTextMessage(recipientId, messageText) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: 'EAACgsZBuhxZA0BAGOXwrfLtb0KvNEq4bY1BNuCxWZCBAvFabV58SJkBnYrspGeuGpJunIQ2ewLAhnK3V5lDUw1jVhWv6glIZBQ9RAZBfpN3xiOcbMFLCjbUqoZAazCSzZAPIxlarXKuQ977wfzXNoYKm0SKgzLJcyujR127HOvZAuAZDZD' },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      console.log("Successfully sent generic message with id %s to recipient %s", 
-        messageId, recipientId);
+app.get('/webhook', function (req, res) {  
+    if (req.query['hub.verify_token'] === 'busboybot') {
+        res.send(req.query['hub.challenge']);
     } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
+        res.send('Invalid verify token');
     }
-  });  
-}
+});
+
+app.post('/webhook', function (req, res) {  
+    var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.message && event.message.text) {
+            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+        }
+    }
+    res.sendStatus(200);
+});
+
+// generic function sending messages
+function sendMessage(recipientId, message) {  
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
 
 app.listen(port);
 console.log("Magic happen in port: " + port);
